f32f3fdccb47abaa8ab9b8c6dbc2c2f634a24bf1
Darrell Nicholas <darrellwnicholas@gmail.com>
2015-01-27 23:28:23 -0600

    Initial Commit

---
+++ UnitTestsCardExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnitTestsCardExample
+//
+//  Created by Darrell Nicholas on 1/27/15.
+//  Copyright (c) 2015 Darrell Nicholas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
